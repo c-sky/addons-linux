cc79411db9ad29bb021d6bdf64d9c2c11a011d8e
lijun <lijun@silan.com.cn>
2017-02-13 14:38:13 +0800

    silan: silan_stmmac added.

---
+++ drivers/net/ethernet/silan_stmmac/silan_gmac_enhance.h
@@ -0,0 +1,32 @@
+/*******************************************************************************
+  This is an enhancement to the STMMAC sriver
+
+  Copyright (C) 2013 Silan Microelectronics Ltd
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms and conditions of the GNU General Public License,
+  version 2, as published by the Free Software Foundation.
+
+  This program is distributed in the hope it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+  more details.
+
+  You should have received a copy of the GNU General Public License along with
+  this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin St - Fifth Floor, Boston, MA 02110-1301 USA.
+
+  The full GNU General Public License is included in this distribution in
+  the file called "COPYING".
+
+  Author: XuHang 
+
+*******************************************************************************/
+#ifndef _SILAN_GMAC_ENHANCE_H
+#define _SILAN_GMAC_ENHANCE_H
+
+/* NAPI state */
+#define NAPI_OFF            0
+#define NAPI_ON             1
+
+#endif /* _SILAN_GMAC_ENHANCE_H */
